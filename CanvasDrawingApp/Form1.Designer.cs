79c27af41b4b8c3a82d663472b27b94ec055afee
prudenceadam <103206638+prudenceadam@users.noreply.github.com>
2024-07-09 15:29:29 +0000

    build canvas drawing

---
+++ CanvasDrawingApp/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CanvasDrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 424);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 20);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Canvas Drawing App";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
---
+++ CanvasDrawingApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CanvasDrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList listOfPoints;
+        private bool pencilDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listOfPoints = new ArrayList();
+            pencilDown = false;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point point = new Point(e.X, e.Y);
+            listOfPoints.Add(point);
+            pencilDown = true;
+            toolStripStatusLabel1.Text = "MouseDown";
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            pencilDown = false;
+            toolStripStatusLabel1.Text = "MouseUp";
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            Point currentPoint = new Point(e.X, e.Y);
+            Pen pencil = new Pen(Color.FromArgb(45, 170, 250), 2); // Change pencil color and thickness
+
+            if (pencilDown)
+            {
+                toolStripStatusLabel1.Text = "MouseMove";
+                if (listOfPoints.Count > 1)
+                {
+                    Point previousPoint = (Point)listOfPoints[listOfPoints.Count - 1];
+                    g.DrawLine(pencil, previousPoint, currentPoint);
+                }
+                listOfPoints.Add(currentPoint);
+            }
+        }
+    }
+}
